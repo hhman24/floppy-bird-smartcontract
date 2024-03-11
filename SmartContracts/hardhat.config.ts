157103e86561ef7e0f640a56f07540d94535d3e9
hhman24 <anhoang483@gmail.com>
2024-03-11 18:15:12 +0700

    hhman - declare .env network

--- SmartContracts/hardhat.config.ts
+++ SmartContracts/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    dev: {
+      url: 'http://localhost:3000',
+      gasPrice: 20,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        count: 10
+      },
+    }
+  }
 };
 
 export default config;
